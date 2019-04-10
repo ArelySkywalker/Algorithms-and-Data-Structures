01674d23ff7a5eb9ba6c2c5b76e9943712196363
Arely Miramontes Rodriguez <arelymts@gmail.com>
2019-04-09 21:14:14 -0500

    Bubble Sort!

---
+++ Section 11 - Intro to Sorting Algorithms/bubbleSort.js
@@ -0,0 +1,23 @@
+/**
+ *  Buble Sort
+ *  - Start by looping from with a variable called i the end of the array towards the beginning
+ *  - Start an inner loop with a variable called j from the beginning until i - 1
+ *  - If arr[j] is greater than arr[j+1], swap those two values!
+ *  - Return the sorted array
+ */
+
+ function mySwap(arr, indx1, indx2) {
+    var temp = arr[indx1];
+    arr[indx1] = arr[indx2];
+    arr[indx2] = temp;
+    return arr;
+ }
+
+ function bubbleSort(arr) {
+     for(var i = arr.length; i > 0; i--) {
+         for(var j = 0; j < i - 1; j++) {
+            if(arr[j] > arr[j+1]) arr = mySwap(arr, j, j+1);
+         }
+     }
+     return arr;
+ }
