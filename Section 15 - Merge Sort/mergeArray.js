294cce59e1ae25478e1cd3677812c32f12f87e48
Arely Miramontes <arelymts@gmail.com>
2019-05-02 21:54:15 -0500

    typo and format

--- Section 15 - Merge Sort/mergeArray.js
+++ Section 15 - Merge Sort/mergeArray.js
@@ -1,14 +1,14 @@
 /**
  *  Merge Function
- *  - Create an empty array, ttake a look at the smallest values in each input array
+ *  - Create an empty array, take a look at the smallest values in each input array
  *  - While there are still values we haven't looked at
- *  - If the value in the first array is smaller than the value in the second array, 
- *      push the value in the first array into our results and move on to the next
- *      value in the first array.
- *  - Else if the value in the first array is large than the value in the second array,
- *      push the value in the second array into our results and move onto the next
- *      value in the second array.
- *  - Once we exhaust one array, push all remaining values from the other array.
+ *      - If the value in the first array is smaller than the value in the second array, 
+ *          push the value in the first array into our results and move on to the next
+ *          value in the first array.
+ *      - Else if the value in the first array is large than the value in the second array,
+ *          push the value in the second array into our results and move onto the next
+ *          value in the second array.
+ *      - Once we exhaust one array, push all remaining values from the other array.
  */
 
 function merge(firstArr, scndArr){
