0f760863e059876a0680907c90ac2068ad3612f4
Arely Miramontes Rodriguez <arelymts@gmail.com>
2019-05-20 19:41:16 -0500

    fix title

--- Section 16 - Quick Sort/quicksort.js
+++ Section 16 - Quick Sort/quicksort.js
@@ -1,5 +1,5 @@
 /**
- *  Pivot Helper
+ *  Quicksort Function
  *  - Call the pivot helper on the array
  *  - When the helper returns to you the updated pivot index, recursively
  *      call the pivot helper on the subarray to the left of that index, and the
