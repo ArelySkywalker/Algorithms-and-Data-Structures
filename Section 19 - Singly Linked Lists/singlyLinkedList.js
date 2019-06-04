8771895e6e3590cdd404b5c0fa41e02a524db886
Arely Miramontes <arelymts@gmail.com>
2019-06-03 21:30:29 -0500

    unshift function

--- Section 19 - Singly Linked Lists/singlyLinkedList.js
+++ Section 19 - Singly Linked Lists/singlyLinkedList.js
@@ -50,6 +50,18 @@ class SlinglyLinkedList {
 		this.length--;
 		return currentHead;
 	}
+	unshift(val) {
+		var newNode = new Node(val);
+		if(!this.head) {
+			this.head = newNode;
+			this.tail = this.head;
+		}else{
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+		this.length++;
+		return this;
+	}
 }
 
 var list = new SlinglyLinkedList();
