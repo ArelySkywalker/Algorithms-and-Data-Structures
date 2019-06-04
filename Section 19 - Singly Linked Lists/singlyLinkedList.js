691f7c5300cfd7692f486957caae7ee8295416e1
Arely Miramontes <arelymts@gmail.com>
2019-06-03 22:09:17 -0500

    insert function

--- Section 19 - Singly Linked Lists/singlyLinkedList.js
+++ Section 19 - Singly Linked Lists/singlyLinkedList.js
@@ -84,6 +84,7 @@ class SlinglyLinkedList {
 		if(index < 0 || index > this.length) return false;
 		if(index === this.length) return !!this.push(val);
 		if(index === 0) return !!this.unshift(val);
+		
 		var newNode = new Node(val);
 		var prev = this.get(index - 1);
 		var temp = prev.next;
