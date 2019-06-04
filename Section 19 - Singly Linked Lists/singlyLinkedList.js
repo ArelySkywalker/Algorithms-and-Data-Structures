db6cc77ee30a7b86107c55a90b9ac34f9e94336b
Arely Miramontes <arelymts@gmail.com>
2019-06-03 22:29:36 -0500

    reversed function

--- Section 19 - Singly Linked Lists/singlyLinkedList.js
+++ Section 19 - Singly Linked Lists/singlyLinkedList.js
@@ -104,6 +104,29 @@ class SlinglyLinkedList {
 		this.length--;
 		return removed;
 	}
+	reverse() {
+		var node = this.head;
+		this.head = this.tail;
+		this.tail = node;
+		var next;
+		var prev = null;
+		for(var i = 0; i < this.length; i++) {
+			next = node.next;
+			node.next = prev;
+			prev = node;
+			node = next;
+		}
+		return this;
+	}
+	print() {
+		var arr = [];
+		var current = this.head;
+		while(current) {
+			arr.push(current.val);
+			current = current.next;
+		}
+		console.log(arr);
+	}
 }
 
 var list = new SlinglyLinkedList();
