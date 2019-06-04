bc9909a9965b768ba54c130b45eb253faa30d952
Arely Miramontes <arelymts@gmail.com>
2019-06-03 21:15:43 -0500

    Pop Function

--- Section 19 - Singly Linked Lists/singlyLinkedList.js
+++ Section 19 - Singly Linked Lists/singlyLinkedList.js
@@ -26,8 +26,25 @@ class SlinglyLinkedList {
 		this.length++;
 		return this;
 	}
+	pop() {
+		if(!this.head) return undefined;
+		var current = this.head;
+		var newTail = current;
+		while(current.next) {
+			newTail = current;
+			current = current.next;
+		}
+		this.tail = newTail;
+		this.tail.next = null;
+		this.length--;
+		if(this.length === 0) {
+			this.head = null;
+			this.tail = null;
+		}
+		return current;
+	}
 }
 
 var list = new SlinglyLinkedList();
-list.push("HELLOW");
+list.push("HELLO");
 list.puah("GOODBYE");
