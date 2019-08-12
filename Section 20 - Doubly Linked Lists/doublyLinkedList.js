3fd04a899543edeb430d7d2b199535800ccb5de1
Arely Miramontes Rodriguez <arelymts@gmail.com>
2019-08-11 19:34:56 -0500

    DLL insert

--- Section 20 - Doubly Linked Lists/doublyLinkedList.js
+++ Section 20 - Doubly Linked Lists/doublyLinkedList.js
@@ -98,4 +98,17 @@ class DoublyLinkedList {
         }
         return false;
     }
+    insert(index, val) {
+        if(index < 0 || index >= this.length) return false;
+        if(index === 0) this.unshift(val);
+        if(index === this.length) this.push(val);
+
+        var newNode = new Node(val);
+        var beforeNode = this.get(index - 1);
+        var afterNode = beforeNode.next;
+        beforeNode.next = newNode, newNode.prev = beforeNode;
+        newNode.next = afterNode, afterNode.prev = newNode;
+        this.length++;
+        return true;
+    }
 }
