04736d5dffe8401ee7b7a0e35d316ddc37314665
Arely Miramontes <arelymts@gmail.com>
2019-08-23 21:04:35 -0500

    depth first search - postorder

--- Section 23 - Tree Traversal/bfs.js
+++ Section 23 - Tree Traversal/bfs.js
@@ -82,6 +82,19 @@ class BinarySearchTree {
 		traverse(this.root);
 		return(data);
 	}
+
+	dfsPostOrder() {
+		var data = [];
+
+		function traverse(node) {
+			if(node.left) traverse(node.left);
+			if(node.right) traverse(node.right);
+
+			data.push(node.value);
+		}
+		traverse(this.root);
+		return data;
+	}
 }
 
 var tree = new BinarySearchTree();
