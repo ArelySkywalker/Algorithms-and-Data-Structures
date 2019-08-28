30357fb25dcbb18bac1c22ab5952d07172c03c5e
Arely Miramontes <arelymts@gmail.com>
2019-08-27 19:06:44 -0500

    Removing from a Heap

--- Section 24 - Binary Heaps/maxBinaryHeap.js
+++ Section 24 - Binary Heaps/maxBinaryHeap.js
@@ -21,8 +21,56 @@ class maxBinaryHeap {
 			let parent = this.values[parentIndex];
 			if(element <= parent) break;
 			this.values[parentIndex] = element;
-			this.values[index] = parent
+			this.values[index] = parent;
 			index = parentIndex;
+		}
+	}
+
+	extractMax() {
+		const max = this.values[0];
+		const end = this.values.pop();
+		this.values[0] = end;
+
+		// trickle down
+		this.sinkDown();
+		
+		return max;
+	}
+
+	sinkDown() {
+		let index = 0;
+		const length = this.values.length;
+		const element = this.values[0];
+		
+		while(true) {
+			let leftChildIdx = 2 * index + 1;
+			let rightChildIdx = 2 * index + 2;
+			let leftChild, rightChild;
+			let swap = null;
+
+			if(leftChildIdx < length) {
+				leftChild = this.values[leftChildIdx];
+				if(leftChild > element) {
+					swap = leftChildIdx;
+				}
+			}
+
+			if(rightChildIdx < length) {
+				rightChild = this.values[rightChildIdx];
+				if(
+					(swap === null && rightChild > element) || 
+					(swap !== null && rightChild > leftChild)
+				) 
+					{
+						swap = rightChildIdx;
+				}
+			}
+
+			if(swap === null) break;
+			this.values[index] = this.values[swap];
+			this.values[swap] = element;
+
+			index = swap;
 		}
 	}
 }
