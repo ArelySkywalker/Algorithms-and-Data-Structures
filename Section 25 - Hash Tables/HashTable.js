a7b11582cb9e44279060aa8c9273562a758b4650
Arely Miramontes <arelymts@gmail.com>
2019-08-28 18:41:59 -0500

    hash table with set and get

---
+++ Section 25 - Hash Tables/HashTable.js
@@ -0,0 +1,45 @@
+class HashTable {
+	constructor(size=53) {
+		this.keyMap = new Array(size);
+	}
+
+	_hash(key) {
+		let total = 0;
+		let WEIRD_PRIME = 31;
+		for (let i = 0; i < Math.min(key.length, 100); i++) {
+			let char = key[i];
+			let value = char.charCodeAt(0) - 96;
+			total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+		}
+		return total;
+	}
+
+	set(key, value) {
+		let index = this._hash(key);
+		if(!this.keyMap[index]) {
+			this.keyMap[index] = [];
+		}
+		this.keyMap[index].push([key, value]);
+	}
+
+	get(key) {
+		let index = this._hash(key);
+		if(this.keyMap[index]) {
+			for(let i = 0; i < this.keyMap[index].length; i++) {
+				if(this.keyMap[index][i][0] === key) {
+					return this.keyMap[index][i][1];
+				}
+			}
+		}
+		return undefined;
+	}
+}
+
+var ht = new HashTable();
+ht.set("maroon","#800000");
+ht.set("yellow","#FFFF00");
+ht.set("olive","#808080");
+ht.set("salmon","#FA8072");
+ht.set("lightcoral","#F08080");
+ht.set("mediumvilotred","#C71585");
+ht.set("plum","#DDA0DD");
