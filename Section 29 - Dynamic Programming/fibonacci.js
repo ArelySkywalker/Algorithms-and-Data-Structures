901482a560dee5870a856f5200be80ce610f2696
Arely Miramontes <arelymts@gmail.com>
2019-08-28 22:26:08 -0500

    memoization

--- Section 29 - Dynamic Programming/fibonacci.js
+++ Section 29 - Dynamic Programming/fibonacci.js
@@ -2,3 +2,12 @@ function fib(n) {
 	if(n <= 2) return 1;
 	return fib(n-1) + fib(n-2);
 }
+
+// Memoization solution
+function fibMemo(n, memo=[]) {
+	if(memo[n] !== undefined) return memo[n];
+	if(n <= 2) return 1;
+	var res = fibMemo(n-1, memo) + fibMemo(n-2, memo);
+	memo[n] = res;
+	return res;
+}
