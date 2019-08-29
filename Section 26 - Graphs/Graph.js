6bcf099e5a215f9ef099e74a26dca7f9f76f7317
Arely Miramontes <arelymts@gmail.com>
2019-08-28 20:54:45 -0500

    graph remove Edge

--- Section 26 - Graphs/Graph.js
+++ Section 26 - Graphs/Graph.js
@@ -17,6 +17,15 @@ class Graph {
 			this.adjacencyList[vertex2].push(vertex1);
 		}
 	}
+
+	removeEdge(vertex1, vertex2) {
+		this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+			v => v !== vertex2
+		);
+		this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+			v => v !== vertex1
+		);
+	}
 }
 
 var gr = new Graph();
